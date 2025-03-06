70285f3b3b74f70a65f5be5816f562f403458f5c
andersonyeo <107699015+andersonyeo@users.noreply.github.com>
2025-03-06 11:13:38 +0800

    Update vehicle_pred_app.py

--- vehicle_pred_app.py
+++ vehicle_pred_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
-
+import numpy as np
 from tensorflow.keras.applications import EfficientNetB1
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping
